f1ef3121fdb247a30d985d5c53aef610e9b66c77
cleiton016 <cleitonluiz2014@gmail.com>
2024-08-30 09:38:00 -0300

    added: create component login

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { ButtonComponent } from "../../shared/components/buttons/button/button.component";
+
+@Component({
+  selector: 'fm-login',
+  standalone: true,
+  imports: [SharedModule, ButtonComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+login($event: SubmitEvent) {
+throw new Error('Method not implemented.');
+}
+
+}
