2e2a000fb6aca3bcb2e9e17528a897455fbf3bf4
cleiton016 <cleitonluiz2014@gmail.com>
2024-08-30 22:00:13 -0300

    fix: add input style

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../shared/components/buttons/button/button.component";
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'fm-register',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, SharedModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
