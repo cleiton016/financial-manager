c577ea871052ad52c1357ae88446a6e3408a2955
cleiton016 <cleitonluiz2014@gmail.com>
2024-08-31 13:57:41 -0300

    fix:  create component navigation

---
+++ src/app/pages/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+
+}
