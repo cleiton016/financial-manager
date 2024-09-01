b3cd97d590af45dc70852b5c124548c852ea589e
cleiton016 <cleitonluiz2014@gmail.com>
2024-08-31 21:41:53 -0300

    fix: add component navigation

--- src/app/pages/navigation/navigation.component.spec.ts
+++ src/app/pages/navigation/navigation.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
+import { WindowRef } from '@services/windowRef';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
+  let windowRefMock: any;
 
   beforeEach(async () => {
+    windowRefMock = {
+      nativeWindow: {
+        matchMedia: jest.fn().mockReturnValue({
+          matches: false
+        })
+      }
+    };
+
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent]
+      imports: [NavigationComponent],
+      providers: [ { provide: WindowRef, useValue: windowRefMock } ]
     })
     .compileComponents();
     
--- src/app/pages/navigation/navigation.component.ts
+++ src/app/pages/navigation/navigation.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { AbstractComponent } from '@components/abstract/abstract.component';
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
   selector: 'fm-navigation',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent extends AbstractComponent {
 
 }
