9eda9138b54726be390393d1b9bccd0e89df72e2
cleiton016 <cleitonluiz2014@gmail.com>
2024-09-12 21:47:21 -0300

    fix: new routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { HomeComponent } from './pages/not-logged-in/home/home.component';
 import { NavigationComponent } from '@pages/navigation/navigation.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'customer-area', component: NavigationComponent},
-    {path: '**', redirectTo: ''}
+    { path: '', component: HomeComponent },
+    { 
+        path: 'customer-area',
+        component: NavigationComponent,
+        loadChildren: () => import('./pages/customer-area/customer-area.module').then(m => m.CustomerAreaModule) 
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
