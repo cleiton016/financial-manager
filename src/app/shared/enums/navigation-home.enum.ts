2f328da15fe8c0c1a9072dfeed74d3bb24be1ea6
cleiton016 <cleitonluiz2014@gmail.com>
2024-09-12 21:29:05 -0300

    fix: refactoring navigation enum

--- src/app/shared/enums/navigation-home.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum NavigationHome {
-    login,
-    register,
-}
--- src/app/shared/utils/menu-navigation.ts
+++ src/app/shared/utils/menu-navigation.ts
@@ -2,17 +2,17 @@ import { TreeMenuItem } from "@shared/interfaces/tree-menu.interface";
 
 export const MENU_NAVIGATION: TreeMenuItem[] = [
     { label: 'Investimentos', icon: 'banknotes' },
-    {
-        label: 'Gastos',
-        icon: 'creditCard',
-    },
+    { label: 'Gestão de Transações', icon: 'receiptLong', children: [
+        { label: 'Transações', icon: 'creditCard', path: 'customer-area/expenses' },
+        { label: 'Categorias', icon: 'category' },
+    ] },
     {
       label: 'Configurações',
       icon: 'settings',
       children: [
         { label: 'Perfil' , icon: 'person' },
         { label: 'Alterar senha', icon: 'lockClosed' },
-        { label: 'Sair', icon: 'logout' }
+        { label: 'Sair', icon: 'logout', action: () => console.log('Sair') },
       ],
     }
 ];
