c21970bed2e17e8857e480e5f6ba9803ae3e9ae1
cleiton016 <cleitonluiz2014@gmail.com>
2024-09-12 21:30:40 -0300

    added: menu component

--- src/app/shared/components/tree-menu/tree-menu.component.ts
+++ src/app/shared/components/tree-menu/tree-menu.component.ts
@@ -3,6 +3,7 @@ import { Component, inject, Input } from '@angular/core';
 import { NgClass, NgIf } from '@angular/common';
 import { MenuService } from '@services/menu.service';
 import { IconComponent } from "../icon/icon.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fm-tree-menu',
@@ -13,17 +14,31 @@ import { IconComponent } from "../icon/icon.component";
   
 })
 export class TreeMenuComponent {
+  router = inject(Router)
   dr = inject(MenuService)
   @Input() menuItems!: TreeMenuItem[];
   selected?: number
+  woldSelected?: TreeMenuItem
 
   toggle(item: TreeMenuItem, id: number): void {
+    if (this.woldSelected) this.woldSelected!.expanded = false;
     this.selected = this.selected === id ? undefined : id;
     item.expanded = !item.expanded;
+    this.woldSelected = item;
+
+    if (item.path) this.navigation(item);
+    if (item.action) item.action();
+
   }
 
   clearSelected(): void {
     this.selected = undefined;
+  }
+
+  navigation(item: TreeMenuItem): void {
+    if (item.path) {
+      this.router.navigate([item.path]);
+    }
   }
 
 }
--- src/app/shared/interfaces/tree-menu.interface.ts
+++ src/app/shared/interfaces/tree-menu.interface.ts
@@ -4,6 +4,8 @@ import { Icons } from "@enums/icons.enum";
 export interface TreeMenuItem {
     label: string;
     icon: Icons;
+    path?: string;
+    action?: () => void;
     children?: TreeMenuItem[];
     expanded?: boolean;
 }
